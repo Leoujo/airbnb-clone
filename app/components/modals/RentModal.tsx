b9160a37e5f997ee6a6e9ab67b085497d6971230
Leonardo Araujo <leoujooficial@gmail.com>
2023-06-01 17:12:30 -0300

    rent modal selecting category

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,6 +5,8 @@ import Modal from "./Modal";
 import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+import { FieldValues, useForm } from "react-hook-form";
 
 // Implementing steps
 enum STEPS {
@@ -20,6 +22,38 @@ const RentModal = () => {
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<FieldValues>({
+    defaultValues: {
+      category: "",
+      location: null,
+      guestCount: 1,
+      roomCount: 1,
+      bathRoomCount: 1,
+      imageSrc: "",
+      price: 1,
+      title: "",
+      description: "",
+    },
+  });
+
+  const category = watch("category");
+
+  // This function forces the custom component to register in react hook form.
+  // We're passing extra information so it reloads the page, like register would.
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldDirty: true,
+      shouldTouch: true,
+      shouldValidate: true,
+    });
+  };
 
   const onBack = () => {
     setStep((value) => value - 1);
@@ -53,20 +87,28 @@ const RentModal = () => {
         className="
 				grid
 				grid-cols-1
+				md:grid-cols-2
 				gap-3
-				max-h[50vh]
-				overflow-y-a
+				max-h-[50vh]
+				overflow-y-auto
 			"
       >
-			{categories.map((item) => (
-				<div key={item.label} className="col-span-1" >
-					{item.label}
-				</div>
-			))}
-		</div>
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={(category) => {
+                setCustomValue("category", category);
+              }}
+              selected={category === item.label}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
-
+ 
   return (
     <Modal
       title="Airbnb your home!"
